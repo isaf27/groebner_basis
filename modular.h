fd3b5a86ab9969df852903c8abb97700030f2bba
Ivan Safonov <isafonov27@gmail.com>
2020-01-02 01:30:05 +0300

    big letter in class name

--- modular.h
+++ modular.h
@@ -4,13 +4,13 @@
 namespace math {
 
     template <uint32_t modulo>
-    class modular {
+    class Modular {
     private:
         uint32_t value;
 
-        modular power(uint32_t degree) const {
+        Modular power(uint32_t degree) const {
             if (degree == 0) {
-                return modular<modulo>(1);
+                return Modular<modulo>(1);
             }
             const auto subpower = this->power(degree / 2);
             if (degree % 2 == 0) {
@@ -20,54 +20,54 @@ namespace math {
             }
         }
 
-        modular inverse() const {
+        Modular inverse() const {
             assert(((void)"division by zero", value != 0));
             return this->power(modulo - 2);
         }
 
     public:
-        modular(uint32_t value = 0) : value(value) {
+        Modular(uint32_t value = 0) : value(value) {
             assert(((void)"value should be less than modulo", value < modulo));
         }
 
-        bool operator==(const modular& other) const {
+        bool operator==(const Modular& other) const {
             return value == other.value;
         }
 
-        modular operator+(const modular& other) const {
+        Modular operator+(const Modular& other) const {
             if (value + other.value >= modulo) {
-                return modular<modulo>(value + other.value - modulo);
+                return Modular<modulo>(value + other.value - modulo);
             } else {
-                return modular<modulo>(value + other.value);
+                return Modular<modulo>(value + other.value);
             }
         }
 
-        modular operator-(const modular& other) const {
+        Modular operator-(const Modular& other) const {
             if (value >= other.value) {
-                return modular<modulo>(value - other.value);
+                return Modular<modulo>(value - other.value);
             } else {
-                return modular<modulo>(value + modulo - other.value);
+                return Modular<modulo>(value + modulo - other.value);
             }
         }
 
-        modular operator*(const modular& other) const {
-            return modular<modulo>((uint64_t)value * (uint64_t)other.value % (uint64_t)modulo);
+        Modular operator*(const Modular& other) const {
+            return Modular<modulo>((uint64_t)value * (uint64_t)other.value % (uint64_t)modulo);
         }
 
-        modular operator/(const modular& other) const {
+        Modular operator/(const Modular& other) const {
             return (*this) * other.inverse();
         }
 
-        friend std::ostream& operator<<(std::ostream &out, const modular &element) {
+        friend std::ostream& operator<<(std::ostream &out, const Modular &element) {
             out << element.value << " (modulo " << modulo << ")";
             return out;
         }
 
-        modular& operator=(modular copy) {
+        Modular& operator=(Modular copy) {
             value = copy.value;
         }
 
-        modular(const modular &copy) : value(copy.value) {
+        Modular(const Modular &copy) : value(copy.value) {
         }
     };
 }
